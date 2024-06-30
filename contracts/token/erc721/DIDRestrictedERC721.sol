8a165ca8e1aabde46c40e28acd794d8d74233a45
Astor Rivera <astordigital@gmail.com>
2024-06-29 20:12:28 -0700

    Added the new did integrated erc721 token and token manager.

---
+++ contracts/token/erc721/DIDRestrictedERC721.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-3
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "../../id/IdentityManager.sol";
+
+contract DIDRestrictedERC721 is ERC721, ERC721Burnable {
+    
+    IdentityManager private identityManager;
+
+    constructor(string memory name, string memory symbol, address identityManagerAddress)
+        ERC721(name, symbol) {
+        identityManager = IdentityManager(identityManagerAddress);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal view {
+        require(tokenId > 0, "Invalid tokenId");
+        // Allow minting and burning (from and to zero address)
+        if (from != address(0) && to != address(0)) {
+            require(identityManager.isOwnerOrDelegate(from, from), "Sender is not a registered DID");
+            require(identityManager.isOwnerOrDelegate(to, to), "Recipient is not a registered DID");
+        }
+    }
+
+    function burn(uint256 tokenId) public override {
+        address owner = ownerOf(tokenId);
+        require(identityManager.isOwnerOrDelegate(owner, owner), "Caller is not token owner or delegate");
+        super.burn(tokenId);
+    }
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+}
---
+++ contracts/token/erc721/ERC721TokenManager.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: GPL-3
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./DIDRestrictedERC721.sol";
+import "../../id/IdentityManager.sol";
+
+contract ERC721TokenManager is OwnableUpgradeable {
+    IdentityManager private identityManager;
+
+    uint256 tokenIndex = 0;
+
+    struct Token {
+        address deployer;
+        string name;
+        string symbol;
+        address tokenAddress;
+    }
+    // Mapping of token address to the token deployment
+    Token[] private tokens;
+
+    event TokenDeployed(address indexed owner, address indexed tokenAddress, string tokenType);
+
+    function initialize(address identityManagerAddress) public initializer {
+        __Ownable_init(msg.sender);
+        identityManager = IdentityManager(identityManagerAddress);
+    }
+
+    function getTokenIndex() public view returns (uint256) {
+        return tokenIndex;
+    }
+
+    function getToken(uint256 index) public view returns(Token memory) {
+        return tokens[index];
+    }
+
+    function deployDIDRestrictedERC721(string memory name, string memory symbol) public {
+        address didDeployer = msg.sender;
+        require(identityManager.isOwnerOrDelegate(didDeployer, didDeployer), "Deployer is not a registered DID");
+        DIDRestrictedERC721 newToken = new DIDRestrictedERC721(name, symbol, address(identityManager));
+
+        address tokenAddress = address(newToken);
+        tokens.push(Token({deployer: didDeployer, name: name, symbol: symbol, tokenAddress: tokenAddress}));
+
+        // increment index
+        tokenIndex++;
+        // emit token reference
+        emit TokenDeployed(didDeployer, tokenAddress, "DIDRestrictedERC721");
+    }
+
+    // Additional functions for managing ERC721 tokens could include transfer of ownership, updating token metadata, etc.
+}
