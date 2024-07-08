9bbf7a872bbd8cfae1fa7cbd0bb2f157ca385205
Astor Rivera <astordigital@gmail.com>
2024-07-08 00:11:10 -0700

    Updated the ERC20SubscriptionManager and supporing unit tests with the IdentityManager integration.

--- contracts/finance/subscription/ERC20SubscriptionManager.sol
+++ contracts/finance/subscription/ERC20SubscriptionManager.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-3
 pragma solidity ^0.8.24;
 
-import "../../access/RoleBasedAccessControl.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "../../did/IdentityManager.sol";
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
@@ -19,7 +21,10 @@ interface IERC20 {
  * @author astor@swtch.network
  * @notice ERC20SubscriptionManager manages ERC20 token based subscription fees.
  */
-contract ERC20SubscriptionManager is RoleBasedAccessControl {
+contract ERC20SubscriptionManager is Initializable, OwnableUpgradeable {
+    
+    IdentityManager public identityManager;
+    
     IERC20 public token;
     uint256 public subscriptionFee;
     uint256 public subscriptionDuration; // in seconds, e.g., 30 days
@@ -35,19 +40,27 @@ contract ERC20SubscriptionManager is RoleBasedAccessControl {
     event SubscriptionRenewed(address indexed user, uint256 newEndTime);
     event SubscriptionCancelled(address indexed user);
 
+    modifier onlyDIDOwner(address did) {
+        require(identityManager.isOwnerOrDelegate(did, msg.sender), "Unauthorized: caller is not the owner or delegate");
+        _;
+    }
+
     /**
-     * @dev Constructor
+     * @dev Initializer
      * @param _tokenAddress Address of the token.
      * @param _fee Service fee.
      * @param _duration Subscription length.
      */
-    constructor(address _tokenAddress, uint256 _fee, uint256 _duration) {
+    function initialize(address _tokenAddress, uint256 _fee, uint256 _duration, address _identityManagerAddress) public initializer {
+        __Ownable_init(msg.sender);
         token = IERC20(_tokenAddress);
         subscriptionFee = _fee;
         subscriptionDuration = _duration;
+
+        identityManager = IdentityManager(_identityManagerAddress);
     }
 
-    function subscribe() public {
+    function subscribe() public onlyDIDOwner(msg.sender) {
         require(token.transferFrom(msg.sender, address(this), subscriptionFee), "Fee transfer failed");
         Subscriber storage user = subscribers[msg.sender];
         require(block.timestamp > user.endTime, "Current subscription must expire before renewal");
@@ -68,9 +81,10 @@ contract ERC20SubscriptionManager is RoleBasedAccessControl {
         return subscribers[user].endTime > block.timestamp;
     }
 
-    function cancelSubscription() public {
+    function cancelSubscription() public onlyDIDOwner(msg.sender) {
         require(subscribers[msg.sender].endTime != 0, "No active subscription");
-        subscribers[msg.sender].endTime = block.timestamp;  // or set to 0 if you prefer to clear data
+         // or set to 0 if you prefer to clear data
+        subscribers[msg.sender].endTime = block.timestamp;
         emit SubscriptionCancelled(msg.sender);
     }
 
@@ -82,13 +96,13 @@ contract ERC20SubscriptionManager is RoleBasedAccessControl {
         subscriptionDuration = newDuration;
     }
 
-    function withdrawTokens(address to, uint256 amount) public onlyOwner {
+    function withdrawTokens(address to, uint256 amount) public onlyDIDOwner(msg.sender) {
         uint256 contractBalance = token.balanceOf(address(this));
         require(amount <= contractBalance, "Withdrawal amount exceeds balance");
         require(token.transfer(to, amount), "Withdrawal transfer failed");
     }
 
-    // utility, move to utils
+    // utility, move to utils|library
     function max(uint a, uint b) private pure returns (uint) {
         return a > b ? a : b;
     }
